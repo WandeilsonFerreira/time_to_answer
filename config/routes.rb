f40dbd8f72dc2fe60912f737cf2fb3c7ec556fe1
WandeilsonFerreira <wandeilson.ferreira@gmail.com>
2019-04-25 11:04:33 -0300

    Portando o tema Gentelella

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   devise_for :admins
-  get 'welcome/index'
-  root to: 'welcome#index'
+  get 'site/welcome/index'
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
