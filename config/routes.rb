fc936ec1155b9681d02abbe60240f40b2491e048
WandeilsonFerreira <wandeilson.ferreira@gmail.com>
2019-05-10 20:20:10 -0300

    Aula 153 - Alterando a senha do admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
+  namespace :admins_backoffice do
+    get 'welcome/index'
+    resources :admins, only: [:index,  :edit, :update]
+  end
   namespace :users_backoffice do
     get 'users/index'
+    resources :users, only: [:index]
   end
   namespace :admins_backoffice do
     get 'admins/index'
   end
   namespace :site do
     get 'welcome/index'
-  end
-  namespace :users_backoffice do
-    get 'welcome/index'
-    resources :users, only: [:index]
-  end
-  namespace :admins_backoffice do
-    get 'welcome/index'
-    resources :admins, only: [:index]
   end
   devise_for :users
   devise_for :admins
